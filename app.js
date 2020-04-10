c11c0c1d468d00443c848f5ef8805a438f5b7233
bhaskar29090 <reddybhaskar437@gmail.com>
2020-04-10 16:21:02 -0500

    commit

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let usersRouter = require('./routes/users');
 
-var app = express();
+let app = express();
 //Set up mongoose connection
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://<Bhaskar>:<kingmaker@1234>@cluster0-dhpyk.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+let mongoose = require('mongoose');
+let mongoDB = 'mongodb+srv://<Bhaskar>:<kingmaker@1234>@cluster0-dhpyk.azure.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true,useUnifiedTopology: true });
-var db = mongoose.connection;
+let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
